1fab91297e4e53cbc03b84676c3f155688c1dfe8
Sachin Jaishankar <sachin.jaishankar@duke.edu>
2018-10-13 16:38:28 -0400

    for luke

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute'])
+
+myApp.config(function ($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: 'pages/clinicregistration.html',
+        controller: 'mainController'
+    })
+    .when('/second', {
+        templateUrl: 'pages/hospitalregistration.html',
+        controller: 'secondController'
+    })
+
+});
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+
+    $scope.handle = '';
+
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$filter', function($scope, $filter) {
 
     $scope.handle = '';
 
